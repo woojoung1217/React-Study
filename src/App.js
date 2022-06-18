4daa21c85d1eb3f372a6a2faa5599ef5a766f3fe
woojoung <ywj981217@naver.com>
2022-06-18 20:11:43 +0900

    used state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts ='가보자 우중아';
 
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','서울 여행기']);
+  let blogName = useState('개발 Blog by React');
+  let [textdate,textdatec] = useState(['2월17일','2월18일','2월19일']);
   return (
     <div className="App">
       <div className="black-nav">
-       <div> 개발Blog</div>
+       <div> {blogName} </div>
+       <div className='sizedbox'><img src ={logo}/></div>
       </div>
-
+      <div className="list">
+        <h3> {  글제목[0] } </h3>
+        <p>{textdate[0]}</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> {  글제목[1] } </h3>
+        <p>{textdate[1]}</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> {  글제목[2] } </h3>
+        <p>{textdate[2]}</p>
+        <hr/>
+      </div>
+     
     </div>
   );
 }
