41ba9db7f046b426a6c9a737994ee8bebf9ea2ad
woojoung <ywj981217@naver.com>
2022-06-18 19:18:13 +0900

    install react , run npm

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts ='가보자 우중아';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+       <div> 개발Blog</div>
+      </div>
+
     </div>
   );
 }
